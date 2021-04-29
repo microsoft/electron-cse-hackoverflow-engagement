a0efe37c27111169704a3856f6f0371f8f3c16a0
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-04-29 14:22:38 -0600

    fix with octoki

--- generate-issues/index.js
+++ generate-issues/index.js
@@ -2,7 +2,7 @@
 
 import dotenv from 'dotenv'
 import { promises as fs } from 'fs'
-import undici from 'undici'
+import { Octokit } from 'octokit'
 
 dotenv.config()
 
@@ -10,26 +10,42 @@ const gh_pat = process.env.gh_pat
 
 const fiddles = await fs.readFile('fiddles', { encoding: 'utf-8' })
 
-const createIssues = fiddles.split('\n').map(fiddle => undici.request(
-  'https://api.github.com/repos/microsoft/electron-cse-hackoverflow-engagement/issues',
-  {
-    method: 'POST',
-    headers: [
-      'user-agent', 'undici',
-      'authorization', `token ${gh_pat}`
-    ],
-    body: JSON.stringify({
-      title: `Update fiddle ${fiddle}`,
-      labels: ['Status: Ready', 'Task'],
-      body: `
+const octokit = new Octokit({ auth: gh_pat })
+
+const issues = await octokit.paginate(octokit.rest.issues.listForRepo, {
+  owner: 'microsoft',
+  repo: 'electron-cse-hackoverflow-engagement'
+})
+
+const existingFiddleIssues = issues.filter(issue => issue.title.includes('Update fiddle')).map(issue => issue.title)
+
+const fiddlesToWrite = fiddles
+  .split('\n')
+  .filter(
+    fiddleName => existingFiddleIssues.every(
+      existingFiddleIssue => !existingFiddleIssue.includes(fiddleName)
+    )
+  )
+
+if (fiddlesToWrite.length > 0) {
+  console.log(`Creating ${fiddlesToWrite.length} issues`)
+  const createIssues = fiddlesToWrite.map(fiddle => octokit.rest.issues.create({
+    title: `Update fiddle ${fiddle}`,
+    labels: ['Status: Ready', 'Task'],
+    body: `
 Template: [${fiddle}](https://github.com/electron/electron/docs/fiddles/${fiddle})
 
 Rel: #6
 
 > This task is for organizational and tracking purposes. Refer to the linked 'Rel:' issues for further details.
 `
-    })
+  }))
+  
+  try {
+    await Promise.all(createIssues)
+  } catch (error) {
+    console.error(error)
   }
-))
-
-await Promise.all(createIssues)
+} else {
+  console.log('No issues to be created')
+}
